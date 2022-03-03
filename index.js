246296028195576efb8ecc27d3c8fec2cf560d55
ryo_of_the_ryo <ryomasumura1201@gmail.com>
2022-03-03 11:24:22 +0900

    メッセージ作成

--- index.js
+++ index.js
@@ -9,19 +9,38 @@ const config = {
 const client = new line.Client(config);
 
 const main = async () => {
+  const week = new Date().getDay();
+
+  console.log(week);
+
+  let text;
+  switch (week) {
+    case 1:
+      text = "明日は燃えるゴミの日です!";
+      break;
+    case 2:
+      text = "明日はペットボトル、缶、びんの日です!";
+      break;
+    case 3:
+      text = "明日は資源ごみの日です!";
+      break;
+  }
+
   const messages = [
     {
       type: "text",
-      text: "いっせい送信です！",
+      text: text,
     },
   ];
 
-  try {
-    const res = await client.broadcast(messages);
-    console.log(res);
-  } catch (error) {
-    console.log(`エラー: ${error.statusMessage}`);
-    console.log(error.originalError.response.data);
+  if (text) {
+    try {
+      const res = await client.broadcast(messages);
+      console.log(res);
+    } catch (error) {
+      console.log(`エラー: ${error.statusMessage}`);
+      console.log(error.originalError.response.data);
+    }
   }
 };
 
